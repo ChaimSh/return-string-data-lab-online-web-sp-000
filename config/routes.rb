b70ab94060cd80183d26e860181c16ff93aa8d55
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-22 03:10:48 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  post 'create', to: 'products#create'
-  get 'inventory', to: 'products#inventory'
-  get 'description', to: 'products#description'
+  resources :products
+  get 'products/:id/inventory', to: 'products#inventory'
+  get 'products/:id/description', to: 'products#description'
 end
